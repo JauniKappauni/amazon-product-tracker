a9a9b671f80346121d8725b4d6740f1d1cec171a
JauniKappauni <social.jauni@gmail.com>
2025-11-05 17:26:58 +0100

    feat: add storing data to local db and displaying data - one data set contains name, price, link and image

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 const express = require("express");
+const sqlite3 = require("sqlite3");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const app = express();
 const port = 3000;
 
+const db = new sqlite3.Database("./products.db");
+
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, link TEXT, price TEXT, image TEXT)"
+  );
+});
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  db.all("SELECT * FROM products", (err, rows) => {
+    res.render("index", { products: rows });
+  });
 });
 
 app.post("/", async (req, res) => {
   const link = req.body.link;
   const response = await axios.get(link);
   const $ = cheerio.load(response.data);
+  const name = $(".a-size-large.product-title-word-break").text().trim();
   const priceWhole = $(".a-price-whole").first().text();
   const priceFraction = $(".a-price-fraction").first().text();
   const priceSymbol = $(".a-price-symbol").first().text();
   var price = priceWhole + priceFraction + priceSymbol;
-  res.send(price);
+  const image = $(".a-dynamic-image").attr("src");
+  db.run(
+    "INSERT INTO products (name, link, price, image) VALUES (?,?,?,?)",
+    [name, link, price, image],
+    (err, rows) => {
+      if (err) {
+        return res.send(err);
+      }
+      res.redirect("/");
+    }
+  );
 });
 
 app.listen(port, () => {
